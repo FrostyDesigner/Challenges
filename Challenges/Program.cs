cf0c5256e03c73ff25c50c529fb956aebece17da
FrostyDesigner <drafter760@gmail.com>
2019-07-26 08:58:03 -0700

    New Challenge

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -12,29 +12,52 @@ namespace Challenges
         static void Main(string[] args)
         {
             // body
-            String a = "EVERY GOOD DOG DESERVES SOME TREATS but sometimes the kitties do too";
 
-            string[] b = a.Split(new[] { " " }, StringSplitOptions.None);
-
-            int count = 0;
-            int wrdCount = 0;
-            string lngWord = string.Empty;
-
-            foreach (var item in b)
-            {
-                count = item.Length;
-                if (count > wrdCount)
-                {
-                    wrdCount = count;
-                    lngWord = item;
-                }
-            }
 
             // return results
-            Console.WriteLine($"Teh longest word is {lngWord.ToLower()}");
+            Console.WriteLine("\nSum of the first 500 prime numbers: ");
+            long sum = 0;
+            int ctr = 0;
+            int n = 2;
+            while (ctr < 10)
+            {
+                if (isPrime(n))
+                {
+                    Console.WriteLine(n);
+                    sum += n;
+                    ctr++;
+                }
+                n++;
+            }
+
+            Console.WriteLine(sum.ToString());
 
             // pause to show results
             Console.ReadLine();
+        }
+
+        public static bool isPrime(int n)
+        {
+            int x = (int)Math.Floor(Math.Sqrt(n));
+
+            if (n == 1)
+            {
+                return false;
+            }
+            if (n == 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i <= x; ++i)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
